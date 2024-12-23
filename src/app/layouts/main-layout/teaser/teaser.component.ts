6c3b2b89037aef797eaba1d6191ac877e4cd45d0
RichardPeda <richard.peda@gmx.net>
2024-12-23 23:47:11 +0100

    main-page video fadeout effect

--- src/app/layouts/main-layout/teaser/teaser.component.ts
+++ src/app/layouts/main-layout/teaser/teaser.component.ts
@@ -1,14 +1,44 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
 import { Video } from '../../../core/models/video';
 import { PlayButtonComponent } from '../../../shared/components/buttons/play-button/play-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-teaser',
   standalone: true,
-  imports: [PlayButtonComponent],
+  imports: [CommonModule, PlayButtonComponent],
   templateUrl: './teaser.component.html',
-  styleUrl: './teaser.component.scss'
+  styleUrl: './teaser.component.scss',
 })
 export class TeaserComponent {
-  video = input<Video>()
+  video = input<Video>();
+  currentVideo: Video | undefined;
+
+  newVideo: Video | undefined;
+  fadeActive = false;
+
+  constructor() {
+    effect(() => {
+      let video = this.video();
+      if (video) {
+        if (this.currentVideo == undefined) {
+          this.currentVideo = video;
+          this.newVideo = video;
+        } else {
+          this.newVideo = video;
+          this.fadeOut(video);
+        }
+      }
+    });
+  }
+
+  fadeOut(video: Video) {
+    this.fadeActive = true;
+    setTimeout(() => {
+      this.currentVideo = video;
+    }, 1500);
+    setTimeout(() => {
+      this.fadeActive = false;
+    }, 3000);
+  }
 }
